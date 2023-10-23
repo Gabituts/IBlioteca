a1ec84e42249cb345cceefdb12469aba530c3901
Gabituts <gabrielbastias25@gmail.com>
2023-10-23 14:10:32 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,23 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
+    path: 'home',loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: 'ibiblioteca',
+    loadChildren: () => import('./pages/ibiblioteca/ibiblioteca.module').then( m => m.IbibliotecaPageModule)
+  },
+  {
+    path: 'autor',
+    loadChildren: () => import('./pages/autor/autor.module').then( m => m.AutorPageModule)
+  },
+  {
+    path: 'curriculum',
+    loadChildren: () => import('./pages/curriculum/curriculum.module').then( m => m.CurriculumPageModule)
   },
 ];
 
